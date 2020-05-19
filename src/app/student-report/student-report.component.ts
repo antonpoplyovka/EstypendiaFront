eed03aa499145661f2bd70702ed9c4e4b39a30c5
antonpoplyovka <antonpoplyovka@gmail.com>
2020-05-19 23:48:09 +0200

    Student report component

---
+++ src/app/student-report/student-report.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from '../services/http.service';
+import {StudentReport} from '../interfaces/studentReport';
+import { formatDate} from '@angular/common';
+
+@Component({
+  selector: 'app-student-report',
+  templateUrl: './student-report.component.html',
+  styleUrls: ['./student-report.component.css']
+})
+export class StudentReportComponent implements OnInit {
+  studentId: number = null;
+  empty = true;
+  studentReport: StudentReport[];
+
+  constructor(private httpService: HttpService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  studentIdReady() {
+    return this.studentId > 0;
+  }
+
+  getStudentReport() {
+    this.studentReport = null;
+    this.httpService.getStudentReport(this.studentId).subscribe(studentTypeController => {
+      this.studentReport = studentTypeController;
+      if (this.studentReport.length === 0) {
+        this.empty = true;
+        console.log(this.studentReport.length);
+      } else {
+        this.empty = false;
+      }
+      console.log(this.studentReport);
+
+    });
+  }
+
+
+
+
+}
